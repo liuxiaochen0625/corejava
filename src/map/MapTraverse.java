982e03f7e4a33e042e10138adc21b89ed22882f1
liuxiaochen0625 <liuxiaoqiang_0625@163.com>
2015-11-10 13:43:30 +0800

    更新map

---
+++ src/map/MapTraverse.java
@@ -0,0 +1,60 @@
+/**
+ *
+ * @author : liuxiaoqiang
+ * @date   :Nov 10, 2015 1:40:03 PM 
+ * @version 1.0 
+ *
+ */
+package map;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ *
+ * @author : liuxiaoqiang
+ * @date   :Nov 10, 2015 11:28:05 AM 
+ * @version 1.0 
+ *
+ */
+
+public class MapTraverse {
+
+	public static void main(String[] args) {  
+
+        HashMap<String,Double> map = new HashMap<String,Double>();  
+        ValueComparator bvc =  new ValueComparator(map);  
+        TreeMap<String,Double> sorted_map = new TreeMap<String,Double>(bvc);  
+
+        map.put("A",99.5);  
+        map.put("B",67.4);  
+        map.put("C",67.4);  
+        map.put("D",67.3);  
+
+        System.out.println("unsorted map: "+map);  
+
+        sorted_map.putAll(map);  
+
+        System.out.println("results: "+sorted_map);  
+    }  
+}
+
+
+class ValueComparator implements Comparator<String> {  
+
+    Map<String, Double> base;  
+    public ValueComparator(Map<String, Double> base) {  
+        this.base = base;  
+    }  
+
+    // Note: this comparator imposes orderings that are inconsistent with equals.      
+    public int compare(String a, String b) {  
+        if (base.get(a) >= base.get(b)) {  
+            return -1;  
+        } else {  
+            return 1;  
+        } // returning 0 would merge keys  
+    }  
+}  
