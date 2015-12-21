346e2b89ffc595c75360e93d1ebd674ea71e02eb
liuxiaochen0625 <liuxiaoqiang_0625@163.com>
2015-12-21 11:30:04 +0800

    we

--- src/util/GoodsList.java
+++ src/util/GoodsList.java
@@ -7,8 +7,13 @@
  */
 package util;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.io.PrintStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,8 +31,15 @@ public class GoodsList {
 		Workbook rwb = null;
 	    Cell cell = null;
 	    InputStream stream = new FileInputStream("E:\\goodsList.xls");
+	    PrintStream writer = new PrintStream(new File("product_id.txt"));
+	    System.setOut(writer);
 	    rwb = Workbook.getWorkbook(stream);
 	    Sheet sheet = rwb.getSheet(0);
+	    String driver = "com.mysql.jdbc.Driver";
+		String url = "jdbc:mysql://localhost:3306/test";
+		Class.forName(driver);
+		Connection conn = DriverManager.getConnection(url, "root", "mysql");
+		Statement sta = conn.createStatement();
 	    for(int i=0; i<sheet.getRows(); i++){
 	    	String[] str = new String[sheet.getColumns()];
 	    	for(int j=0; j<sheet.getColumns(); j++){
@@ -36,13 +48,21 @@ public class GoodsList {
 	    	}
 	    	list.add(str);
 	    }
-	    for(int i=0;i<list.size();i++){
+	    for(int i=1;i<list.size();i++){
 	        String[] str = (String[])list.get(i);
-	        for(int j=0;j<str.length;j++){
-	        	System.out.print(str[j]+"\t");
+	        String sql = "insert into i_product values("+str[0];
+	        for(int j=1;j<52;j++){
+	        	sql = sql + ",'"+str[j]+"'";
 	        }
-	        System.out.println();
+	        sql = sql + ")";
+	        try{
+	        	sta.execute(sql);
+	        }catch(Exception e){
+	        	writer.println(str[0]);
+	        	e.printStackTrace();
+	        }
 	    }
+	    sta.close();
+	    conn.close();
 	}
-
 }
