d34f3ae83785ee4c5b7e91dfdd27e28ecd6017e0
liuxiaochen0625 <liuxiaoqiang_0625@163.com>
2015-12-29 11:15:04 +0800

    ip

--- src/util/Utils.java
+++ src/util/Utils.java
@@ -18,6 +18,8 @@ import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 
 public class Utils {
 	//在每行前面添加一个/
@@ -109,5 +111,19 @@ public class Utils {
 			throw new Exception("Read file failed", e);
 		}
 		return ret;
+	}
+	
+	public synchronized static String getIpAddr(HttpServletRequest request) {
+		String ip = request.getHeader("x-forwarded-for");
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("Proxy-Client-IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getHeader("WL-Proxy-Client-IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getRemoteAddr();
+		}
+		return ip;
 	}
 }
