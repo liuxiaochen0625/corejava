da2a94adeb9f6b4d23c0d06e7abbf4de85d41bb6
liuxiaochen0625 <liuxiaoqiang_0625@163.com>
2016-02-18 17:07:31 +0800

    Huffman

---
+++ src/interfaces/Tree.java
@@ -0,0 +1,20 @@
+/**
+ *
+ * @author : liuxiaoqiang
+ * @date   :Feb 18, 2016 4:51:12 PM 
+ * @version 1.0 
+ *
+ */
+package interfaces;
+
+public class Tree {
+	private Node root;
+	 
+    public Node getRoot() {
+            return root;
+    }
+
+    public void setRoot(Node root) {
+            this.root = root;
+    }
+}
