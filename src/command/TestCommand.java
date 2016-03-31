7b83c776a5822bcf3c8187770b069cf552da21ee
liuxiaochen0625 <liuxiaoqiang_0625@163.com>
2016-03-31 10:29:59 +0800

    java执行exe文件

--- src/command/TestCommand.java
+++ src/command/TestCommand.java
@@ -7,16 +7,28 @@
  */
 package command;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+
+import util.util;
+
 public class TestCommand {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		List<File> listFile = util.getFiles("D:\\release");
 		Runtime runtime = Runtime.getRuntime();
 		Process process = null;
-		String command = "D:\\tools\\name.bat";
-		try {
-			process = runtime.exec(command);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		System.out.println(process.getOutputStream().toString());
+		for(File file :listFile)
+			if(file.getName().equals("1.jpg")){
+				String command = "D:\\release\\zxing.exe --try-harder "+file.getAbsolutePath();
+				process = runtime.exec(command);
+				BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+				String str = "";
+				while((str = reader.readLine())!=null)
+					System.out.println(str);
+				System.out.println(file.getAbsolutePath());
+			}
 	}
 }
